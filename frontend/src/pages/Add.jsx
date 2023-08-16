f79d4d4415241dff038cf5e036cf4d440bec9211
vinayaka-iyer <vinayakaiyer999@gmail.com>
2023-08-16 21:46:45 +0530

    Updates

--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "./styles.scss";
 
 const Add = () => {
   const [anim, setAnim] = useState({
